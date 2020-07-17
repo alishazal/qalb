56d543a0be689a61f152a2fc0fd62b44559049dd
Ali Shazal <alishazal@Alis-MacBook-Air.local>
2020-07-18 01:56:04 +0400

    added dalma_scripts

---
+++ dalma_scripts.sh
@@ -0,0 +1,18 @@
+# ---- Working on July 18, 2020 ----
+# Script 1: word2word-train-predict-evaluate.sh
+
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH -p nvidia
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=as10505
+#SBATCH --mem=30000
+#SBATCH --time=24:00:00
+module purge
+module load all
+module load anaconda/2-4.1.1
+module load cuda/8.0
+module load gcc/4.9.3
+source activate capstone-gpu
+
+python3 transliterate.py
